877f56e3fddd53ec12a07fda2a7c0a71ec8320bd
Sunil Sharma P <sunil.sharma@pathpartnertech.com>
2018-04-23 10:18:10 +0530

    Stattic Library creation in Linux
    * How to create static library
    * How to use static library
    
    Signed-off-by: Sunil Sharma P <sunil.sharma@pathpartnertech.com>

---
+++ c/static_lib/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "static.h"
+
+int main()
+{
+    printf("Sum of 10 and 20     : %d\n", add2(10, 20));
+    printf("Sum of 10, 20 and 30 : %d\n", add3(10, 20, 30));
+    return 0;
+}
