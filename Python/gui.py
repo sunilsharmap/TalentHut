74c199ff919ff51316330efd621a2c1577a5efe9
Sunil Sharma P <sunillsharma.pv@gmail.com>
2019-05-16 10:02:30 +0530

    Python package updated
    Removed *~ and *.pyc files
    
    Signed-off-by: Sunil Sharma P <sunillsharma.pv@gmail.com>

--- Python/gui.py
+++ Python/gui.py
@@ -6,7 +6,10 @@ def pressed():
 
 
 root=Tk()
-root.title('Python GUI')
+root.title('PY - GUI')
+root.eval('tk::PlaceWindow %s center' % root.winfo_pathname(root.winfo_id()))
+root.resizable(width=False, height=False)
+root.geometry('125x50')
 
 first=Frame(root)
 first.pack()
---
+++ Python/python_package/python_package/first_module.py
@@ -0,0 +1,2 @@
+def call_first_module():
+	print ("\nFirst module")
