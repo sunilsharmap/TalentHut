b049b7b85c539e358471ada99952d840a70408e4
Sunil Sharma P <sunillsharma.pv@gmail.com>
2019-08-02 08:01:45 +0530

    Makefile - To compile kernel module, application
    netlink_user.c modified
    
    Signed-off-by: Sunil Sharma P <sunillsharma.pv@gmail.com>

--- C_Programming/Netlink_socket/netlink_user.c
+++ C_Programming/Netlink_socket/netlink_user.c
@@ -1,74 +1,63 @@
 /** Code taken from 
-  * "https://stackoverflow.com/questions/3299386/how-to-use-netlink-socket-to-communicate-with-a-kernel-module"
-  * Details
-  * http://opensourceforu.com/2015/08/netlink-a-communication-mechanism-in-linux/
-  */
+ * "https://stackoverflow.com/questions/3299386/how-to-use-netlink-socket-to-communicate-with-a-kernel-module"
+ * Details
+ * http://opensourceforu.com/2015/08/netlink-a-communication-mechanism-in-linux/
+ */
 
-#include <linux/module.h>
-#include <net/sock.h> 
 #include <linux/netlink.h>
-#include <linux/skbuff.h> 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
 
 #define NETLINK_USER 31
 
-struct sock *nl_sk = NULL;
+#define MAX_PAYLOAD 1024 /* maximum payload size*/
+struct sockaddr_nl src_addr, dest_addr;
+struct nlmsghdr *nlh = NULL;
+struct iovec iov;
+int sock_fd;
+struct msghdr msg;
 
-static void hello_nl_recv_msg(struct sk_buff *skb)
+int main()
 {
-	struct nlmsghdr *nlh;
-	int pid;
-	struct sk_buff *skb_out;
-	int msg_size;
-	char *msg = "Hello from kernel";
-	int res;
+	sock_fd = socket(PF_NETLINK, SOCK_RAW, NETLINK_USER);
+	if (sock_fd < 0)
+		return -1;
 
-	printk(KERN_INFO "Entering: %s\n", __FUNCTION__);
+	memset(&src_addr, 0, sizeof(src_addr));
+	src_addr.nl_family = AF_NETLINK;
+	src_addr.nl_pid = getpid(); /* self pid */
 
-	msg_size = strlen(msg);
+	bind(sock_fd, (struct sockaddr *)&src_addr, sizeof(src_addr));
 
-	nlh = (struct nlmsghdr *)skb->data;
-	printk(KERN_INFO "Netlink received msg payload:%s\n", (char *)nlmsg_data(nlh));
-	pid = nlh->nlmsg_pid; /*pid of sending process */
+	memset(&dest_addr, 0, sizeof(dest_addr));
+	dest_addr.nl_family = AF_NETLINK;
+	dest_addr.nl_pid = 0; /* For Linux Kernel */
+	dest_addr.nl_groups = 0; /* unicast */
 
-	skb_out = nlmsg_new(msg_size, 0);
-	if (!skb_out) {
-		printk(KERN_ERR "Failed to allocate new skb\n");
-		return;
-	}
+	nlh = (struct nlmsghdr *)malloc(NLMSG_SPACE(MAX_PAYLOAD));
+	memset(nlh, 0, NLMSG_SPACE(MAX_PAYLOAD));
+	nlh->nlmsg_len = NLMSG_SPACE(MAX_PAYLOAD);
+	nlh->nlmsg_pid = getpid();
+	nlh->nlmsg_flags = 0;
 
-	nlh = nlmsg_put(skb_out, 0, 0, NLMSG_DONE, msg_size, 0);
-	NETLINK_CB(skb_out).dst_group = 0; /* not in mcast group */
-	strncpy(nlmsg_data(nlh), msg, msg_size);
+	strcpy(NLMSG_DATA(nlh), "Hello");
 
-	res = nlmsg_unicast(nl_sk, skb_out, pid);
-	if (res < 0)
-		printk(KERN_INFO "Error while sending bak to user\n");
+	iov.iov_base = (void *)nlh;
+	iov.iov_len = nlh->nlmsg_len;
+	msg.msg_name = (void *)&dest_addr;
+	msg.msg_namelen = sizeof(dest_addr);
+	msg.msg_iov = &iov;
+	msg.msg_iovlen = 1;
+
+	printf("Sending message to kernel\n");
+	sendmsg(sock_fd, &msg, 0);
+	printf("Waiting for message from kernel\n");
+
+	/* Read message from kernel */
+	recvmsg(sock_fd, &msg, 0);
+	printf("Received message payload: %s\n", (char *)NLMSG_DATA(nlh));
+	close(sock_fd);
 }
-
-static int __init hello_init(void)
-{
-	struct netlink_kernel_cfg cfg = {
-		.input = hello_nl_recv_msg,
-	};
-
-	printk("Entering: %s\n", __FUNCTION__);
-	//nl_sk = netlink_kernel_create(&init_net, NETLINK_USER, 0, hello_nl_recv_msg, NULL, THIS_MODULE);
-	nl_sk = netlink_kernel_create(&init_net, NETLINK_USER, &cfg);
-	if (!nl_sk) {
-		printk(KERN_ALERT "Error creating socket.\n");
-		return -10;
-	}
-
-	return 0;
-}
-
-static void __exit hello_exit(void)
-{
-	printk(KERN_INFO "exiting hello module\n");
-	netlink_kernel_release(nl_sk);
-}
-
-module_init(hello_init);
-module_exit(hello_exit);
-
-MODULE_LICENSE("GPL");
