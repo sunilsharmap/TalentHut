270055901013e71ae080ba43efbc441475f6eb75
Sunil Sharma P <sunilsharmap@eximiusdesign.com>
2020-04-26 11:55:10 +0530

    Debug Printf: Code change
    Revisited and fixed issued

--- C_Programming/debug_printf/function.c
+++ C_Programming/debug_printf/function.c
@@ -3,7 +3,7 @@
 int call_main()
 {
     printf("This is Printf\n");
-    debug_printf("Testing function call........@ Line: %s\n", __FILE__);
+    debug_printf("Testing function call........@ Line: %d\n", __LINE__);
     printf("This is Printf\n");
     debug_printf("Line: %d Func: %s File: %s\n", __LINE__, __func__, __FILE__);
     return 0;
--- C_Programming/debug_printf/main.c
+++ C_Programming/debug_printf/main.c
@@ -1,10 +1,11 @@
 #include "header.h"
 
 extern int call_main();
+
 int main()
 {
     printf("This is Printf\n");
-    debug_printf("This is debug_printf @ Line: %s%s\n", __FILE__, __func__);
+    debug_printf("This is debug_printf @ Line: %s : %s\n", __FILE__, __func__);
     printf("This is Printf\n");
     debug_printf("This is debug_printf @ Line: %d\n", __LINE__);
     call_main();
