cff3b2590f32e8a58be43cea31d7b8d66dd36e70
Sunil Sharma P <sunilsharma.pv@gmail.com>
2020-05-22 19:04:29 +0530

    dep Package: Creating .deb package for debian
    - Helloword .deb package creation
    - Helloword .deb package installation
    - Helloword .deb package uninstallation / remove

---
+++ C_Programming/debian_package/helloworld_src/main.c
@@ -0,0 +1,19 @@
+/*
+ * main.c
+ *
+ * Description: Creating .deb package for debian distro
+ *              This is helloword deb package
+ *
+ *  Created on: 22-May-2020
+ *      Author: Sunil Sharma P <sunilsharma.pv@gmail.com>
+ *
+ *     Version: 1.0v
+ */
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	printf("Hello World!!\n");
+	return 0;
+}
