db60bd019b17209fef77b3de7d4f3d83e604a8b6
Sunil Sharma P <sunilsharma.pv@gmail.com>
2020-06-11 13:20:07 +0530

    Update main.c

--- C_Programming/encapsulation_datahiding/main.c
+++ C_Programming/encapsulation_datahiding/main.c
@@ -4,15 +4,16 @@ int main()
 {
     Test *st_ptr = NULL;
     st_ptr = create_st();
-    if(!st_ptr)
+    if(!st_ptr) {
         return -1;
+    }
 
-	// This is the hiding, directly you cannot access the members of ``Test`` structure.
-	// Use ``set/get`` api's to access the Structure Test
+    // This is the hiding, directly you cannot access the members of ``Test`` structure.
+    // Use ``set/get`` api's to access the Structure Test
 
-	// st_ptr->id = 10;  // Error
+    // st_ptr->id = 10;  // Error
 
-	set_id(st_ptr, 10);
+    set_id(st_ptr, 10);
 
     // printf("ID = %d\n", st_ptr->id);  // Error
 
