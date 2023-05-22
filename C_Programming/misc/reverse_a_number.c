00dd5537ab56de5b1b12fc0cca31867115f40ac1
Sunil Sharma P <sunilsharma.pv@gmail.com>
2023-05-22 11:35:01 +0530

    C program to reverse a number

---
+++ C_Programming/misc/reverse_a_number.c
@@ -0,0 +1,16 @@
+// C program to reverse a number
+
+#include <stdio.h>
+int main()
+{
+	int n = 123456789, reverse = 0;
+	printf("Before Reverse = %d\n", n);
+
+	while (n != 0) {
+		reverse = (reverse * 10) + (n % 10);
+		n = n / 10;
+	}
+
+	printf("After Reverse  = %d", reverse);
+	return 0;
+}
