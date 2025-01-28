f3f49f3c76cae1f77860ff468b433b1846975479
Sunil Sharma P <sunilsharma.pv@gmail.com>
2025-01-28 12:55:50 +0530

    Update second_smallest_in_array.c

--- C_Programming/misc/second_smallest_in_array.c
+++ C_Programming/misc/second_smallest_in_array.c
@@ -4,48 +4,31 @@
 
 int main()
 {
-	/* Sorted array */
-    int arr[] = {1, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    int size = 10;
-    int first, second, i = 0;
-	first = second = INT_MIN;
+	int arr[] = { 3, -2, 0, 10, 1, 2, 3, -4, 5, 6 };
+	int size = 10;
+	int first=INT_MAX, second=INT_MAX, i = 0;
 
-	if(size < 2) {
-        printf("Array size is less than two, second smallest doesnot exist\n");
-		return 0;
-    }
-	
-	/* while() is to find the same samll numbers in the sorted array */
-	while(i < (size - 1)) {
-		/* Compare first two indices of array and update the first and second */
-		if(arr[i] != arr[i + 1]) {
-			if (arr[i] < arr[i + 1]) {
-				first = arr[i];
-				second = arr[i + 1];
-			}
-			else {
-				first = arr[i + 1];
-				second = arr[i];
-			}
-		}
-
-		i++;
+	if (size < 2) {
+		printf("Array size is less than two, second smallest doesnot exist\n");
+		return -1;
 	}
 
-	for(i = 0; i < size; i++) {
-		if(arr[i] < first) {
+	for (i; i < size; i++) {
+		if (arr[i] < first) {
 			second = first;
 			first = arr[i];
 		}
-		else if((arr[i] != first) && (arr[i] < second)) {
+		else if ((arr[i] < second) && (arr[i] != first)) {
 			second = arr[i];
 		}
 	}
 
-	if (second == INT_MAX) { 
-		printf("There is no second smallest element\n");
+	if (second == INT_MAX) {
+		printf("There is no second smallest elemen\n");
 	}
 	else {
 		printf("The second Smallest element is %d\n", second);
 	}
+
+	return 0;
 }
